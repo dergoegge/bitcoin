647a0bb95ed52cac436803bc271733819f344565
dergoegge <n.goeggi@gmail.com>
2022-02-08 22:31:56 +0100

    Squashed 'src/utreexo/' content from commit 375b2d85b
    git-subtree-dir: src/utreexo
    git-subtree-split: 375b2d85bd11bf86a5047c7ddc61537e8af42db6

---
+++ include/pollard.h
@@ -0,0 +1,56 @@
+#ifndef UTREEXO_POLLARD_H
+#define UTREEXO_POLLARD_H
+
+#include "accumulator.h"
+
+namespace utreexo {
+
+class Pollard : public Accumulator
+{
+private:
+    class InternalNode;
+    /* Pollards implementation of Accumulator::Node */
+    class Node;
+
+    NodePtr<Pollard::InternalNode> m_remember;
+
+    /*
+     * Return the node and its sibling.
+     * Point path to the parent of the node. The path to the node can be traversed in reverse order using the
+     * Accumulator::Node::Parent function.
+     */
+    std::vector<NodePtr<Pollard::InternalNode>> Read(uint64_t pos, NodePtr<Accumulator::Node>& path, bool record_path) const;
+
+    NodePtr<Accumulator::Node> SwapSubTrees(uint64_t from, uint64_t to) override;
+    NodePtr<Accumulator::Node> MergeRoot(uint64_t parent_pos, Hash parent_hash) override;
+    NodePtr<Accumulator::Node> NewLeaf(const Leaf& hash) override;
+    void FinalizeRemove(uint64_t next_num_leaves) override;
+
+    void InitChildrenOfComputed(NodePtr<Pollard::Node>& node,
+                                NodePtr<Pollard::Node>& left_child,
+                                NodePtr<Pollard::Node>& right_child,
+                                bool& recover_left,
+                                bool& recover_right);
+
+    bool CreateProofTree(std::vector<NodePtr<Node>>& proof_tree,
+                         std::vector<std::pair<NodePtr<Node>, int>>& recovery,
+                         const BatchProof& proof);
+
+    bool VerifyProofTree(std::vector<NodePtr<Pollard::Node>> proof_tree,
+                         const std::vector<Hash>& target_hashes,
+                         const std::vector<Hash>& proof_hashes);
+
+public:
+    Pollard(const std::vector<Hash>& roots, uint64_t num_leaves);
+    Pollard(uint64_t num_leaves);
+    ~Pollard();
+
+    bool Prove(BatchProof& proof, const std::vector<Hash>& target_hashes) const override;
+    bool Verify(const BatchProof& proof, const std::vector<Hash>& target_hashes) override;
+
+    /** Prune everything except the roots. */
+    void Prune();
+};
+
+};     // namespace utreexo
+#endif // UTREEXO_POLLARD_H
---
+++ src/test/state_tests.cpp
@@ -0,0 +1,82 @@
+#include "../state.h"
+#include <boost/test/unit_test.hpp>
+
+using namespace utreexo;
+
+BOOST_AUTO_TEST_SUITE(state_tests)
+
+BOOST_AUTO_TEST_CASE(constructor)
+{
+    ForestState state;
+    BOOST_CHECK(state.m_num_leaves == 0);
+    ForestState state1(100);
+    BOOST_CHECK(state1.m_num_leaves == 100);
+}
+
+BOOST_AUTO_TEST_CASE(positions)
+{
+    /*
+     * 28
+     * |---------------\
+     * 24              25              26
+     * |-------\       |-------\       |-------\
+     * 16      17      18      19      20      21      22
+     * |---\   |---\   |---\   |---\   |---\   |---\   |---\
+     * 00  01  02  03  04  05  06  07  08  09  10  11  12  13  14
+    */
+
+    ForestState state(15);
+
+    BOOST_CHECK(state.LeftChild(28) == 24);
+    BOOST_CHECK(state.Sibling(state.LeftChild(28)) == 25);
+    BOOST_CHECK(state.RightSibling(state.LeftChild(28)) == 25);
+    BOOST_CHECK(state.RightSibling(25) == 25);
+    BOOST_CHECK(state.Parent(state.LeftChild(28)) == 28);
+
+    for (uint64_t pos = 0; pos < 8; pos++) {
+        BOOST_CHECK(state.Ancestor(pos, 3) == 28);
+        BOOST_CHECK(state.LeftDescendant(state.Ancestor(pos, 3), 3) == 0);
+    }
+
+    BOOST_CHECK(state.LeftDescendant(26, 2) == 8);
+    BOOST_CHECK(state.LeftDescendant(25, 2) == 4);
+    BOOST_CHECK(state.Cousin(4) == 6);
+    BOOST_CHECK(state.Cousin(5) == 7);
+}
+
+BOOST_AUTO_TEST_CASE(proof)
+{
+    /*
+     * 28
+     * |---------------\
+     * 24              25              26
+     * |-------\       |-------\       |-------\
+     * 16      17      18      19      20      21      22
+     * |---\   |---\   |---\   |---\   |---\   |---\   |---\
+     * 00  01  02  03  04  05  06  07  08  09  10  11  12  13  14
+    */
+
+    ForestState state(15);
+
+    std::vector<uint64_t> targets = {0};
+    std::vector<uint64_t> expected_proof = {1, 17, 25};
+    std::vector<uint64_t> expected_computed = {0, 16, 24, 28};
+    std::pair<std::vector<uint64_t>, std::vector<uint64_t>> output = state.ProofPositions(targets);
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected_proof.begin(), expected_proof.end(),
+                                  output.first.begin(), output.first.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected_computed.begin(), expected_computed.end(),
+                                  output.second.begin(), output.second.end());
+
+    targets = {0, 2, 3, 6, 8, 10, 11};
+    expected_proof = {1, 7, 9, 18};
+    expected_computed = {0, 2, 3, 6, 8, 10, 11, 16, 17, 19, 20, 21, 24, 25, 26, 28};
+    output = state.ProofPositions(targets);
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected_proof.begin(), expected_proof.end(),
+                                  output.first.begin(), output.first.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected_computed.begin(), expected_computed.end(),
+                                  output.second.begin(), output.second.end());
+
+    // TODO: add tests with random numbers
+}
+
+BOOST_AUTO_TEST_SUITE_END()
