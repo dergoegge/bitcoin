dc5bdfd550243948cd5f7ebf45abc696bff925ef
Pieter Wuille <pieter@wuille.net>
2023-05-02 07:58:17 -0400

    Add macros for fuzz targets that return FuzzResult

--- src/test/fuzz/fuzz.h
+++ src/test/fuzz/fuzz.h
@@ -34,7 +34,7 @@ using FuzzBufferType = Span<const uint8_t>;
 using TypeTestOneInput = std::function<FuzzResult(FuzzBufferType)>;
 
 struct FuzzTargetOptions {
-    std::function<void()> init{[] { }};
+    std::function<void()> init{[] {}};
     bool hidden{false};
 };
 
@@ -42,24 +42,36 @@ void FuzzFrameworkRegisterTarget(std::string_view name, TypeTestOneInput target,
 
 #if defined(__clang__)
 #define FUZZ_TARGET(...) _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wgnu-zero-variadic-macro-arguments\"") DETAIL_FUZZ(__VA_ARGS__) _Pragma("clang diagnostic pop")
+#define FUZZ_PARTIAL_TARGET(...) _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wgnu-zero-variadic-macro-arguments\"") DETAIL_FUZZ_PARTIAL(__VA_ARGS__) _Pragma("clang diagnostic pop")
 #else
 #define FUZZ_TARGET(...) DETAIL_FUZZ(__VA_ARGS__)
+#define FUZZ_PARTIAL_TARGET(...) DETAIL_FUZZ_PARTIAL(__VA_ARGS__)
 #endif
 
-#define DETAIL_FUZZ(name, ...)                                                        \
-    FuzzResult name##_fuzz_target(FuzzBufferType);                                    \
-    void name##_fuzz_target_complete(FuzzBufferType);                                 \
-    struct name##_Before_Main {                                                       \
-        name##_Before_Main()                                                          \
-        {                                                                             \
-            FuzzFrameworkRegisterTarget(#name, name##_fuzz_target, {__VA_ARGS__});    \
-        }                                                                             \
-    } const static g_##name##_before_main;                                            \
-    FuzzResult name##_fuzz_target(FuzzBufferType buffer)                              \
-    {                                                                                 \
-        name##_fuzz_target_complete(buffer);                                          \
-        return FuzzResult::MAYBE_INTERESTING;                                         \
-    }                                                                                 \
+#define DETAIL_FUZZ(name, ...)                                                     \
+    FuzzResult name##_fuzz_target(FuzzBufferType);                                 \
+    void name##_fuzz_target_complete(FuzzBufferType);                              \
+    struct name##_Before_Main {                                                    \
+        name##_Before_Main()                                                       \
+        {                                                                          \
+            FuzzFrameworkRegisterTarget(#name, name##_fuzz_target, {__VA_ARGS__}); \
+        }                                                                          \
+    } const static g_##name##_before_main;                                         \
+    FuzzResult name##_fuzz_target(FuzzBufferType buffer)                           \
+    {                                                                              \
+        name##_fuzz_target_complete(buffer);                                       \
+        return FuzzResult::MAYBE_INTERESTING;                                      \
+    }                                                                              \
     void name##_fuzz_target_complete(FuzzBufferType buffer)
+
+#define DETAIL_FUZZ_PARTIAL(name, ...)                                             \
+    FuzzResult name##_fuzz_target(FuzzBufferType);                                 \
+    struct name##_Before_Main {                                                    \
+        name##_Before_Main()                                                       \
+        {                                                                          \
+            FuzzFrameworkRegisterTarget(#name, name##_fuzz_target, {__VA_ARGS__}); \
+        }                                                                          \
+    } const static g_##name##_before_main;                                         \
+    FuzzResult name##_fuzz_target(FuzzBufferType buffer)
 
 #endif // BITCOIN_TEST_FUZZ_FUZZ_H
