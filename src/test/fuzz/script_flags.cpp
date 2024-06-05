fe827e90e28ce9fabe9b2f99dcfa7d8f226fc64b
dergoegge <n.goeggi@gmail.com>
2024-06-05 14:02:55 +0100

    [fuzz] Add script_flags_mocked harness

--- src/test/fuzz/script_flags.cpp
+++ src/test/fuzz/script_flags.cpp
@@ -15,7 +15,53 @@
 #include <utility>
 #include <vector>
 
-FUZZ_TARGET(script_flags)
+namespace {
+class FuzzedSignatureChecker : public BaseSignatureChecker
+{
+public:
+    FuzzedSignatureChecker(const CTransaction* tx, unsigned int in,
+                           const CAmount& amount, const PrecomputedTransactionData& tx_data,
+                           MissingDataBehavior mdb) {}
+
+    bool CheckECDSASignature(const std::vector<unsigned char>& sig, const std::vector<unsigned char>& pub_key,
+                             const CScript& script_code, SigVersion sig_version) const override
+    {
+        return !sig.empty() && sig[0] & 1;
+    }
+    bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pub_key,
+                               SigVersion sig_version, ScriptExecutionData& exec_data,
+                               ScriptError* script_error = nullptr) const override
+    {
+        constexpr std::array<ScriptError, 3> schnorr_errs = {
+            SCRIPT_ERR_SCHNORR_SIG,
+            SCRIPT_ERR_SCHNORR_SIG_SIZE,
+            SCRIPT_ERR_SCHNORR_SIG_HASHTYPE};
+        if (sig.size() < 2) {
+            if (script_error) {
+                *script_error = schnorr_errs[sig.empty() ? 0 : sig[0] % schnorr_errs.size()];
+            }
+            return false;
+        }
+
+        bool sig_ok = sig[0] & 1;
+        if (!sig_ok && script_error) {
+            *script_error = schnorr_errs[sig[1] % schnorr_errs.size()];
+        }
+
+        return sig_ok;
+    }
+    bool CheckLockTime(const CScriptNum& lock_time) const override
+    {
+        return lock_time.getint() & 1;
+    }
+    bool CheckSequence(const CScriptNum& sequence) const override
+    {
+        return sequence.getint() & 1;
+    }
+};
+
+template <typename SigChecker>
+void CheckScriptFlags(FuzzBufferType buffer)
 {
     if (buffer.size() > 100'000) return;
     DataStream ds{buffer};
@@ -45,7 +91,7 @@ FUZZ_TARGET(script_flags)
 
         for (unsigned i = 0; i < tx.vin.size(); ++i) {
             const CTxOut& prevout = txdata.m_spent_outputs.at(i);
-            const TransactionSignatureChecker checker{&tx, i, prevout.nValue, txdata, MissingDataBehavior::ASSERT_FAIL};
+            const SigChecker checker{&tx, i, prevout.nValue, txdata, MissingDataBehavior::ASSERT_FAIL};
 
             ScriptError serror;
             const bool ret = VerifyScript(tx.vin.at(i).scriptSig, prevout.scriptPubKey, &tx.vin.at(i).scriptWitness, verify_flags, checker, &serror);
@@ -68,4 +114,20 @@ FUZZ_TARGET(script_flags)
     } catch (const std::ios_base::failure&) {
         return;
     }
+}
+} // namespace
+
+
+// Both of the following harnesses test that all script verification flags only
+// tighten the interpreter rules (i.e. they represent soft-forks).
+
+FUZZ_TARGET(script_flags)
+{
+    CheckScriptFlags<TransactionSignatureChecker>(buffer);
+}
+
+// Signature validation is mocked out through FuzzedSignatureChecker
+FUZZ_TARGET(script_flags_mocked)
+{
+    CheckScriptFlags<FuzzedSignatureChecker>(buffer);
 }
